1ef57a5a57a6f98b4a48a0434552d72d856cf937
Aloysius Riki <68075271+aloysius-riki@users.noreply.github.com>
2022-06-19 18:15:46 +0300

    implement delete functionality

--- restaurant_api/views.py
+++ restaurant_api/views.py
@@ -19,7 +19,7 @@ def restaurant_list(request):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(['GET', 'PUT'])
+@api_view(['GET', 'PUT', 'DELETE'])
 def restaurant_detail(request, id):
 
     try:
@@ -37,3 +37,7 @@ def restaurant_detail(request, id):
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    elif request.method == 'DELETE':
+        restaurant.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
